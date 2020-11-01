1e60916c02249e18267f28c986efca927a3c9898
aki158 <hideponpon1931@gmail.com>
2020-11-01 16:39:18 +0900

    [終了確認]参照1

--- javascript-essence-tutorial-master/040_変数/920_修了確認_参照１/start/main.js
+++ javascript-essence-tutorial-master/040_変数/920_修了確認_参照１/start/main.js
@@ -7,7 +7,8 @@ let val = 0;
 let val2 = val;
 val2 = 1;
 
-// console.log(val); // 問題１
+console.log(val); // 問題１
+//   0 ◯
 
 let obj = {
     prop1: 10
@@ -15,7 +16,9 @@ let obj = {
 const obj2 = obj;
 obj2.prop1 = 9;
 
-// console.log(obj); // 問題２
+console.log(obj); // 問題２
+//  {prop1: 10}　×　オブジェクトはプロパティを参照しているだけなのでコピーすると同じ場所を見てる。
+//  {prop1: 9}
 
 function fn1(arg) {
     arg.prop2 = 8;
@@ -23,7 +26,9 @@ function fn1(arg) {
 }
 let obj3 = fn1(obj2);
 
-// console.log(obj); // 問題３
+console.log(obj); // 問題３
+//  {prop1: 10} ×
+//  {prop1: 9, prop2: 8}
 
 function fn2(arg) {
     arg = {};
@@ -31,4 +36,12 @@ function fn2(arg) {
 }
 const obj4 = fn2(obj); // 問題４
 
-// console.log(obj); // 問題４
+console.log(obj); // 問題４
+//   {} ×
+//   {prop1: 9, prop2: 8}
+
+let obj5 = obj;
+この時点ではobj5とobjの参照先は一緒
+obj5 = {};
+// obj5の参照先が{}に変更される
+// objは変わんない
