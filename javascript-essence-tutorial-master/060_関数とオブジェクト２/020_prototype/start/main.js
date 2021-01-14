36380ae7e23635ea7b8761bcbd3c88d3d218ac4e
aki158 <hideponpon1931@gmail.com>
2021-01-14 10:07:26 +0900

    79 prototype

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/020_prototype/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/020_prototype/start/main.js
@@ -1,8 +1,25 @@
 function Person(name, age) {
     this.name = name;
     this.age = age;
+    // 下の書き方でもできる
+    // this.hello = function() {
+    //     console.log('hello ' + this.name);
+    // }
+}
+
+// prototype.helloとすることでhelloメソッドを定義できる
+Person.prototype.hello = function() {
+    console.log('hello ' + this.name);
 }
 
 const bob = new Person('Bob', 18);
 const tom = new Person('Tom', 33);
 const sun = new Person('Sun', 20);
+
+// .helloで呼び出せる
+bob.hello();
+tom.hello();
+
+// プロトタイプを使う理由
+// Personを呼び出すたびにhelloを呼び出すので余分なメモリを消費する
+// prototypeであれば__proto__を参照して一つにまとまるから余分なメモリを消費しない
