f84422213a123eb13b00a4d8453dc392d5971ab9
aki158 <hideponpon1931@gmail.com>
2021-01-18 22:43:33 +0900

    83 プロトタイプチェーン

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
@@ -1,11 +1,24 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
+  // this.hello = function() {
+  //   // 優先度1
+  //   console.log('OwnProperty: hello ' + this.name);
+  // }
 }
 
 Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+  // 優先度2
+  console.log('Person: hello ' + this.name);
+}
+
+Object.prototype.hello = function() {
+  // 優先度3
+  console.log('Object: hello ' + this.name);
 }
 
 const bob = new Person('Bob', 18);
 bob.hello();
+
+const result = bob.hasOwnProperty('name');
+console.log(result);
