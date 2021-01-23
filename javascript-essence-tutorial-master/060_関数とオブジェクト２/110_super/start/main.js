ced5ec6a930adee89729000d6ac2641c39fc7009
aki158 <hideponpon1931@gmail.com>
2021-01-23 15:44:23 +0900

    88 super

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/110_super/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/110_super/start/main.js
@@ -27,17 +27,27 @@ class Japanese extends Person {
 
 const taro = new Japanese('Taro', 23, 'Male');
 console.log(taro);
-taro.bye();
+taro.hello();
 
-// const american = {
-//     hello() {
-//         console.log('hello ' + this.name);
-//     }
-// }
+const american = {
+    hello() {
+        super.hello();
+        // console.log('hello ' + this.name);
+    }
+}
 
-// const bob = {
-//     name: 'Bob',
-//     hello() {
-//         super.hello();
-//     }
-// }
+const bob = {
+    name: 'Bob',
+    // hello() {
+    //     console.log('hello ' + this.name);
+    //     // super.hello();
+    // }
+}
+// 適用されない
+bob.bye = function() {
+    super.hello();
+}
+
+Object.setPrototypeOf(bob, american);
+
+bob.hello();
