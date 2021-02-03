f7635ccab2c44269fda964c9a7705e822bfa3f6c
aki158 <hideponpon1931@gmail.com>
2021-02-03 22:20:37 +0900

    94 チェーンメソッド

--- javascript-essence-tutorial-master/060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
+++ javascript-essence-tutorial-master/060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
@@ -6,14 +6,17 @@ class Person {
 
 	hello(person) {
 		console.log(`${this.name} says hello ${person.name}`);
+		return this;
 	}
 
 	introduce() {
 		console.log(`Hi, I'm ${this.name}, ${this.age} years old.`);
+		return this;
 	}
 
 	shakeHands(person) {
 		console.log(`${this.name} shake hands with ${person.name}.`);
+		return this;
 	}
 
 	bye(person) {
@@ -25,6 +28,10 @@ const bob = new Person('Bob', 23);
 const tim = new Person('Tim', 33);
 
 bob.hello(tim);
+	.introduce();
+	.shakeHands(tim);
+	.bye(tim);;
+
 bob.introduce();
 bob.shakeHands(tim);
 bob.bye(tim);;
